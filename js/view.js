5efd0b17d9b9169a574419690bd61b4577e68ecc
arnautfdez <arnau.torrents.fernandez@gmail.com>
2021-04-04 12:55:41 +0200

    edit todo

--- js/view.js
+++ js/view.js
@@ -1,11 +1,15 @@
 import AddTodo from "./components/add-todo.js";
+import Modal from "./components/modal.js";
 
 export default class View {
     constructor() {
         this.model = null;
         this.table = document.getElementById('table');
         this.addTodoForm = new AddTodo();
+        this.modal = new Modal();
+
         this.addTodoForm.onClick((title, description) => this.addTodo(title, description));
+        this.modal.onClick((id, values) => this.editTodo(id, values));
     }
 
     setModel(model) {
@@ -26,6 +30,15 @@ export default class View {
         this.model.toggleCompleted(id);
     }
 
+    editTodo(id, values) {
+        this.model.editTodo(id, values);
+        const row = document.getElementById(id);
+        row.children[0].innerText = values.title;
+        row.children[1].innerText = values.description;
+        row.children[2].children[0].checked = values.completed;
+    }
+
+
     removeTodo(id) {
         this.model.removeTodo(id);
         document.getElementById(id).remove();
@@ -41,9 +54,7 @@ export default class View {
                 
             </td>
             <td class="text-right">
-                <button class="btn btn-primary mb-1">
-                    <i class="fa fa-pencil"></i>
-                </button>
+               
             </td>
         `;
 
@@ -52,6 +63,14 @@ export default class View {
         checkbox.checked = todo.completed;
         checkbox.onclick = () => this.toggleCompleted(todo.id);
         row.children[2].appendChild(checkbox);
+
+        const editBtn = document.createElement('button');
+        editBtn.classList.add('btn', 'btn-primary', 'mb-1');
+        editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
+        editBtn.setAttribute('data-toggle', 'modal');
+        editBtn.setAttribute('data-target', '#modal');
+        editBtn.onclick = () => this.modal.setValues(todo);
+        row.children[3].appendChild(editBtn);
 
         const removeBtn = document.createElement('button');
         removeBtn.classList.add('btn', 'btn-danger', 'mb-1', 'ml-1');
